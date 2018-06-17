c5ad2ed922925f21840d0940a561a6d1e91a830a
Piotr Furmanek <pete85@users.noreply.github.com>
2018-06-17 18:03:40 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { SpeciesComponent } from './species/species.component';
+import { PlanetsComponent } from './planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FilmsComponent,
+    PeopleComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    SpeciesComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    HomeComponent,
+    FilmsComponent,
+    PeopleComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    SpeciesComponent,
+    PlanetsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
